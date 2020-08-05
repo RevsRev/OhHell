cf89a89004c4340cb18470447e34373415f5a973
Eddie Revell <53410878+RevsRev@users.noreply.github.com>
2020-08-05 13:37:06 +0100

    Add files via upload

---
+++ OhHell.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class OhHell {
+
+  private JFrame ohHellFrame = new JFrame("Oh Hell!");
+  private OhHellPanel ohHellPanel;
+
+  public OhHell(int numPlayers, int numRounds) {
+    ohHellPanel = new OhHellPanel(numPlayers, numRounds);
+    ohHellFrame.setSize(500,500);
+    ohHellFrame.add(ohHellPanel);
+    ohHellFrame.setLayout(null);
+    ohHellFrame.setVisible(true);
+    ohHellFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+
+  public static void main(String []args) {
+    OhHell ohHell = new OhHell(4,12);
+  }
+
+}
