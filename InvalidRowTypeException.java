ffc69ddf9dd9e87e8c56ad80d099b908fe90d628
Eddie Revell <53410878+RevsRev@users.noreply.github.com>
2020-08-08 17:36:54 +0100

    Add files via upload

---
+++ InvalidRowTypeException.java
@@ -0,0 +1,10 @@
+public class InvalidRowTypeException extends Exception {
+
+  public InvalidRowTypeException(String message) {
+    super(message);
+  }
+
+  public InvalidRowTypeException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
